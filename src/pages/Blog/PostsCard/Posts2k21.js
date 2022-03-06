530556c080cbc480a554b761978313505b774177
Jeferson Veiga <jeferson.veiiga@icloud.com>
2022-03-06 16:02:24 -0300

    new-features

--- src/pages/Blog/PostsCard/Posts2k21.js
+++ src/pages/Blog/PostsCard/Posts2k21.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Footer from 'components/Footer/Footer'
 
 
 
@@ -10,12 +9,11 @@ const PagesBlogPostsCard = () => {
       <main className="container-post__title">
         <span className="container-title__card">2021</span>
         <Link to="/Async-Await">
-          <section className="container-card__async-await">
+          <section className="container-card">
             <h2>Promises and Async/Await</h2>
             <p>17 Novembro 2021</p>
           </section>
         </Link>
-        <Footer/>
       </main>
     );
 }
